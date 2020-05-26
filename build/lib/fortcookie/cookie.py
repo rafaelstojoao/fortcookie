3b1c8b6e84e12f40a43355bdf3d048b8d8dbb8e7
Rafael Stoffalette Joao <rafaelstojoao@gmail.com>
2020-05-26 15:59:54 -0300

    vrersion 0.0.3 alpha

--- build/lib/fortcookie/cookie.py
+++ build/lib/fortcookie/cookie.py
@@ -1,17 +1,19 @@
-class FortuneCookie:
+# class FortuneCookie:
 
-    def __init__(self):
-        with open('../dataset/mymind.dat') as ds:
-            self.lines = ds.readlines()
-        self.myfortune = ""
+    # def __init__(self):
+with open('../dataset/mymind.dat') as ds:
+    lines = ds.readlines()
+    myfortune = ""
 
-    def creck(self):
+    def creck():
         import random
-        key = random.randint(0,len(self.lines))
-        print(self.lines[key])
+        key = random.randint(0,len(lines))
+        print(lines[key])
         return
 
 # if __name__ == '__main__':
-cookie = FortuneCookie()
+# cookie = FortuneCookie()
 # cookie.creck()
 
+
+creck()
