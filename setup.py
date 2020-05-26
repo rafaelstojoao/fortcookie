7fa0d2d766a1254700a0e3efb7e2917795584b6f
Rafael Stoffalette Joao <rafaelstojoao@gmail.com>
2020-05-26 12:38:40 -0300

    att

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fortcookie", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Rafael Stoffalette Joao",
     author_email="rafaelstojoao@gmail.com",
     description="Say cookie.crack() and be lucky...",
