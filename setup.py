ed92266f106de7e8bf51f6eb6a5c8ed320879d5a
Rafael Stoffalette Joao <rafaelstojoao@gmail.com>
2020-05-26 12:19:00 -0300

    update1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Say cookie.crack() and be lucky...",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/rafaelstojoao/fortcookie",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
